4d00f226b3166a97bb69b28314f9c595f3318c9e
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2013-05-08 20:22:07 +0200

    move meta to hemis-theme

--- hemisu-theme.el
+++ hemisu-theme.el
@@ -1,4 +1,31 @@
-(require 'hemisu-dark-theme)
+;;; hemisu.el --- Hemisu for Emacs.
+
+;; based on Hemisu vim theme https://github.com/noahfrederick/Hemisu
+;; of Noah Frederick
+
+;; Copyright (C) 2013 Andrzej Sliwa
+
+;; Author: Andrzej Sliwa
+;; URL: http://github/anrzejsliwa/django-theme
+;; Version: 1.0.0
+;;
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
 (require 'hemisu-light-theme)
+(require 'hemisu-dark-theme)
 
 (provide 'hemisu-theme)
+
+;;; hemisu-theme.el ends here
